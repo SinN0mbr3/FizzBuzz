a50181cdebcd8381f715af75fe971fff4d71b3a6
SinNombre <reyesmauro1123@icloud.com>
2024-08-22 04:31:15 +0200

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,7 +2,7 @@ i = 0
 
 while i < 100:  # indico que el ciclo se termine en cuanto i>100
     i = i+1  # le sumo 1 en cada ciclo sino seria infinito
-    if i % 3 == 0 and i % 5 == 0:  # declaro todo lo que quiero que pase cuando x condicion paseS
+    if i % 3 == 0 and i % 5 == 0:  # declaro todo lo que quiero que pase cuando x condicion pase
         print("FIZZBUZZ")
     elif i % 3 == 0:
         print("FIZZ")
